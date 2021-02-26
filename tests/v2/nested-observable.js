809bcd89bb1a78bb3db3eacbc06f63ca7c64985a
Matt Nawrocki <mjnrock@umich.edu>
2021-02-25 21:56:53 -0500

    init

---
+++ tests/v2/nested-observable.js
@@ -0,0 +1,25 @@
+import Observer from "../../src/v2/Observer";
+import Observable from "../../src/v2/Observable";
+import Value from "./Value";
+import Experience from "./Experience";
+
+// const value = new Value(5, { min: 0, max: 10 });
+const value = new Experience(5);
+const ob = Observable.Factory({
+    value: value,
+});
+// console.log(value)
+// console.log(value.toData())
+
+const obs = new Observer(ob);
+obs.on("next", (...args) => console.log(`NEXT`, ...args));
+
+ob.value.current = 1500;
+ob.value.current = 2000;
+
+console.log(ob.value.total)
+
+
+// console.log(value)
+// console.log(ob)
+// console.log(ob.cats)
