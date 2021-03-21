adf4097f3285f2dc30c4fa9ea3cd8a88ec535ea6
Matt Nawrocki <mjnrock@umich.edu>
2021-03-21 11:01:24 -0400

    init

---
+++ src/v4/index.js
@@ -0,0 +1,13 @@
+import Util from "./util/package";
+
+import Watchable from "./Watchable";
+import Watcher from "./Watcher";
+import EventWatchable from "./EventWatchable";
+
+export default {
+    Util,
+    
+    Watchable,
+    Watcher,
+    EventWatchable,
+};
