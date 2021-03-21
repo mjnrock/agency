aa417c34722d42dd6750ad5f53282d08095a56e5
Matt Nawrocki <mjnrock@umich.edu>
2021-03-21 16:13:05 -0400

    init

--- src/v4/index.js
+++ src/v4/index.js
@@ -3,6 +3,8 @@ import Util from "./util/package";
 import Watchable from "./Watchable";
 import Watcher from "./Watcher";
 import EventWatchable from "./EventWatchable";
+import Pulse from "./Pulse";
+import Registry from "./Registry";
 
 export default {
     Util,
@@ -10,4 +12,6 @@ export default {
     Watchable,
     Watcher,
     EventWatchable,
+    Pulse,
+    Registry,
 };
