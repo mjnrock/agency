9504033c1b63db76f7cf741c5ce4c1f68834bb4a
Matt Nawrocki <mjnrock@umich.edu>
2021-03-25 08:51:44 -0400

    init

--- tests/v4/util.dice.js
+++ tests/v4/util.dice.js
@@ -1,6 +1,52 @@
 import Util from "../../src/v4/util/package";
 
-let results = [ 0, 0, 0, ];
+
+console.log("------------");
+
+let percento = [ 0, 0, ];
+for(let i = 0; i < 10000; i++) {
+    const result = Util.Dice.percento();
+
+    result >= 0.50 ? ++percento[ 0 ] : ++percento[ 1 ];
+}
+console.log(percento)    //  ev: approximately [ 5000, 5000 ]
+
+
+console.log("------------");
+
+let permille = [ 0, 0, ];
+for(let i = 0; i < 10000; i++) {
+    const result = Util.Dice.permille();
+
+    result >= 0.500 ? ++permille[ 0 ] : ++permille[ 1 ];
+}
+console.log(permille)    //  ev: approximately [ 5000, 5000 ]
+
+
+console.log("------------");
+
+let coins = [ 0, 0, ];
+for(let i = 0; i < 10000; i++) {
+    const coin = Util.Dice.coin();
+
+    coin ? ++coins[ 0 ] : ++coins[ 1 ];
+}
+console.log(coins)    //  ev: approximately [ 5000, 5000 ]
+
+
+console.log("------------");
+
+let coins2 = [ 0, 0, ];
+for(let i = 0; i < 10000; i++) {
+    const coin2 = Util.Dice.coin2();
+
+    coin2 ? ++coins2[ 0 ] : ++coins2[ 1 ];
+}
+console.log(coins2)    //  ev: approximately [ 5000, 5000 ]
+
+console.log("------------");
+
+let weighted = [ 0, 0, 0, ];
 for(let i = 0; i < 10000; i++) {
     const result = Util.Dice.weighted([
         5,
@@ -13,18 +59,18 @@ for(let i = 0; i < 10000; i++) {
     ]);
 
     if(result === "cat1") {
-        ++results[ 0 ];
+        ++weighted[ 0 ];
     } else if(result === "cat2") {
-        ++results[ 1 ];
+        ++weighted[ 1 ];
     } else if(result === "cat3") {
-        ++results[ 2 ];
+        ++weighted[ 2 ];
     }
 }
-console.log(results)    //  ev: approximately [ 2000, 6000, 2000 ]
+console.log(weighted)    //  ev: approximately [ 2000, 6000, 2000 ]
 
 console.log("------------");
 
-let results2 = [ 0, 0, 0, ];
+let weighted2 = [ 0, 0, 0, ];
 for(let i = 0; i < 10000; i++) {
     const result = Util.Dice.weighted2([
         [ 10, "cat11" ],
@@ -33,11 +79,11 @@ for(let i = 0; i < 10000; i++) {
     ]);
 
     if(result === "cat11") {
-        ++results2[ 0 ];
+        ++weighted2[ 0 ];
     } else if(result === "cat22") {
-        ++results2[ 1 ];
+        ++weighted2[ 1 ];
     } else if(result === "cat33") {
-        ++results2[ 2 ];
+        ++weighted2[ 2 ];
     }
 }
-console.log(results2)    //  ev: approximately [ 2000, 6000, 2000 ]
+console.log(weighted2)    //  ev: approximately [ 2000, 6000, 2000 ]
