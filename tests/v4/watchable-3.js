017b774e1601e948bf3d862ead3e7a1e2b39927b
Matt Nawrocki <mjnrock@umich.edu>
2021-03-28 12:47:08 -0400

    init

--- tests/v4/watchable-3.js
+++ tests/v4/watchable-3.js
@@ -23,47 +23,49 @@ ob.obj.a = new Watchable({
     }
 });
 
+// let run  = false;
+// const pulse = new Pulse(1, { autostart: true });
+// pulse.$.subscribe(function(prop, value) {
+//     run = !run;
+
+//     if(run) {
+//         // ob.obj.a.inside = 3;
+//         // ob.obj.a.nested.nested1 = 3;
+//         // ob.obj.a.nested.layer1.layer2 = 3;
+//         ob.obj.a.nested.layer1.layer2.sup = 3;
+//     } else {
+//         // ob.obj.a.inside = 5;
+//         // ob.obj.a.nested.nested1 = 5;
+//         // ob.obj.a.nested.layer1.layer2 = 5;
+//         ob.obj.a.nested.layer1.layer2.sup = 5;
+//     }
+
+//     // console.clear();
+//     // console.log([ ...ob.__subscribers.keys() ]);
+//     // console.log([ ...ob.obj.__subscribers.keys() ]);
+// });
+
 let run  = false;
 const pulse = new Pulse(1, { autostart: true });
 pulse.$.subscribe(function(prop, value) {
     run = !run;
 
     if(run) {
-        // ob.obj.a.inside = 3;
-        // ob.obj.a.nested.nested1 = 3;
-        // ob.obj.a.nested.layer1.layer2 = 3;
-        ob.obj.a.nested.layer1.layer2.sup = 3;
+        ob.reg.register(new Watchable({
+            nested: {
+                layer1: {
+                    layer2: {
+                        sup: 0
+                    }
+                }
+            }
+        }), "test");
     } else {
-        // ob.obj.a.inside = 5;
-        // ob.obj.a.nested.nested1 = 5;
-        // ob.obj.a.nested.layer1.layer2 = 5;
-        ob.obj.a.nested.layer1.layer2.sup = 5;
+        ob.reg.unregister("test")
     }
 
-    // console.clear();
-    // console.log([ ...ob.__subscribers.keys() ]);
-    // console.log([ ...ob.obj.__subscribers.keys() ]);
+    console.clear();
+    console.log([ ...ob.__subscribers.keys() ]);
+    console.log([ ...ob.reg.__subscribers.keys() ]);
+    console.log(ob.reg.keys);
 });
-// const pulse = new Pulse(1, { autostart: true });
-// pulse.$.subscribe(function(prop, value) {
-//     run = !run;
-
-//     if(run) {
-//         ob.reg.register(new Watchable({
-//             nested: {
-//                 layer1: {
-//                     layer2: {
-//                         sup: 0
-//                     }
-//                 }
-//             }
-//         }), "test");
-//     } else {
-//         ob.reg.unregister("test")
-//     }
-
-//     console.clear();
-//     console.log([ ...ob.__subscribers.keys() ]);
-//     console.log([ ...ob.reg.__subscribers.keys() ]);
-//     console.log(ob.reg.keys);
-// });
