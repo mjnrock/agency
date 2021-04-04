92f440de9988ed04d6a3daae409bd4176fbdf246
Matt Nawrocki <mjnrock@umich.edu>
2021-04-04 12:13:01 -0400

    init

--- src/event/package.js
+++ src/event/package.js
@@ -1,7 +1,13 @@
 import Emitter from "./Emitter";
 import Network from "./Network";
+import Router from "./Router";
+import Channel from "./Channel";
+import EventBus from "./EventBus";
 
 export default {
     Emitter,
     Network,
+    EventBus,
+    Channel,
+    Router,
 };
