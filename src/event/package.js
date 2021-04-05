11a9de3c228649e90a717d6e44e94f6de9ebe9ca
Matt Nawrocki <mjnrock@umich.edu>
2021-04-05 12:06:06 -0400

    init

--- src/event/package.js
+++ src/event/package.js
@@ -3,9 +3,15 @@ import Context from "./Context";
 import Router from "./Router";
 import Network from "./Network";
 
+import $EventReceiver from "./$EventReceiver";
+import $EventSender from "./$EventSender";
+
 export default {
     Emitter,
     Context,
     Router,
     Network,
+
+    $EventReceiver,
+    $EventSender,
 };
