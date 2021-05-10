40323d551f8521d916bb823da67d0d3f1ac7e7c8
Matt Nawrocki <mjnrock@umich.edu>
2021-05-10 11:03:16 -0400

    init

--- src/modules/react/package.js
+++ src/modules/react/package.js
@@ -1,30 +1,7 @@
-/* eslint-disable */
-import { useState, useEffect } from "react";
-
-import Network from "../../event/Network";
 import ReactNetwork from "./ReactNetwork";
-
-export function useNetwork(network) {
-    const [ state, setState ] = useState(() => network.state);
-    const [ dispatcher ] = useState(() => new Dispatcher(network.network, network.network));
-
-    useEffect(() => {
-        const handler = function([ state ]) {
-            setState(state);
-        };
-        
-        network.getChannel("_internal").addHandler(Network.Signals.UPDATE, handler);
-        
-        return () => {
-            network.getChannel("_internal").removeHandler(Network.Signals.UPDATE, handler);
-        };
-    }, []);
-    
-    return [ state, dispatcher.dispatch, dispatcher.broadcast ];
-};
+import useNetwork from "./useNetwork";
 
 export default {
-    useNetwork,
-    
     ReactNetwork,
+    useNetwork,
 };
---
+++ src/modules/react/useNetwork.js
@@ -0,0 +1,56 @@
+import { useState, useEffect, useContext } from "react";
+
+import Network from "../../event/Network";
+import Dispatcher from "../../event/Dispatcher";
+
+/**
+ * Create a React Hook that will get the state of the passed network
+ *  and return it, along with a dispatcher attached to that same network,
+ *  using the network as its subject.
+ */
+export function useNetwork(network) {
+    const [ state, setState ] = useState(() => network.state);
+    const [ dispatcher ] = useState(() => new Dispatcher(network, network));
+
+    useEffect(() => {
+        const handler = function({ data }) {
+            const [ newState ] = data;
+
+            setState(newState);
+        };
+        
+        network.getChannel("_internal").addHandler(Network.Signals.UPDATE, handler);
+        
+        return () => {
+            network.getChannel("_internal").removeHandler(Network.Signals.UPDATE, handler);
+        };
+    }, [ network ]);
+    
+    return {
+        state,
+        dispatch: dispatcher.dispatch,
+        broadcast: dispatcher.broadcast,
+    };
+};
+
+/**
+ * A wrapper for << useNetwork >> for cases where the network resides within
+ *  a React Context.  @prop can be used with dot notation to grab a nested value.
+ */
+export function useContextNetwork(context, prop = "network") {
+    const ctx = useContext(context);
+
+    let nested = prop.split("."),
+        network = ctx;
+
+    for(let p of nested) {
+        network = network[ p ];
+    }
+
+    return useNetwork(network);
+};
+
+export default {
+    useNetwork,
+    useContextNetwork,
+};
