3165718b3ab7aadd7f65cc19c87bdfd26ef00bad
Matt Nawrocki <mjnrock@umich.edu>
2021-05-23 12:53:18 -0400

    init

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import Util from "./util/package";
 import Logic from "./logic/package";
 
 import Event from "./event/package";
-import Watchable from "./watchable/package";
 
 import Registry from "./Registry";
+import AgencyBase from "./AgencyBase";
 
 export default {
     Util,
     Logic,
 
     Event,
-    Watchable,
 
     Registry,
+    AgencyBase,
 }
