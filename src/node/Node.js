0fb8eca756b14965e17a88002e3cb3458d885278
Matt Nawrocki <mjnrock@umich.edu>
2021-06-27 13:44:22 -0400

    init

--- src/node/Node.js
+++ src/node/Node.js
@@ -3,10 +3,14 @@ import { isDeepStrictEqual } from "util";
 import AgencyBase from "../AgencyBase";
 
 export class Node extends AgencyBase {
-	constructor({ state = {}, reducers = [], listeners = [], meta = {}, config = {} } = {}) {
+	constructor({ state = {}, reducers = [], listeners = [], meta = {}, config = {}, children = [], refs = [] } = {}) {
 		super();
 
-		this.meta = meta;
+		this.meta = {
+			children: new Set(children),
+			refs: new Map(refs),
+			...meta,
+		};
 		this.state = state;
 
 		this.reducers = new Set(reducers);
@@ -18,8 +22,40 @@ export class Node extends AgencyBase {
 		};
 	}
 
+	get children() {
+		return this.meta.children;
+	}
+	child(index = 0) {
+		if(Array.isArray(index)) {
+			return Object.fromEntries(index.map(i => [ i, this.child(i) ]));
+		}
+
+		return [ ...this.meta.children ][ index ];
+	}
+
+	get refs() {
+		return this.meta.refs;
+	}
+	ref(key) {
+		if(Array.isArray(key)) {
+			return Object.fromEntries(key.map(k => [ k, this.ref(k) ]));
+		}
+
+		return this.meta.refs.get(key);
+	}
+
 	toggle(entry) {
 		this.config[ entry ] = !this.config[ entry ];
+
+		return this;
+	}
+	flagOn(entry) {
+		this.config[ entry ] = true;
+		
+		return this;
+	}
+	flagOff(entry) {
+		this.config[ entry ] = false;
 
 		return this;
 	}
@@ -35,29 +71,69 @@ export class Node extends AgencyBase {
 		return this.reducers.delete(reducer);
 	}
 
-	link(node) {
+	attach(node) {
+		if(node instanceof Node) {
+			this.meta.children.add(node);
+		}
+
+		return this;
+	}
+	attachMany(connectArgs = []) {
+		for(let args of connectArgs) {
+			this.attach(...args);
+		}
+
+		return this;
+	}
+	detach(node) {
+		if(node instanceof Node) {
+			return this.meta.children.delete(node);
+		}
+
+		return false;
+	}
+	detachMany(disconnectArgs = []) {
+		for(let args of disconnectArgs) {
+			this.detach(...args);
+		}
+
+		return this;
+	}
+
+	listen(node) {
 		this.listeners.add(node);
 
 		return this;
 	}
-	linkMany(linkArgs = []) {
+	listenMany(linkArgs = []) {
 		for(let args of linkArgs) {
-			this.link(...args);
+			this.listen(...args);
 		}
 
 		return this;
 	}
-	unlink(node) {
+	unlisten(node) {
 		return this.listeners.delete(node);
 	}
-	unlinkMany(unlinkArgs = []) {
+	unlistenMany(unlinkArgs = []) {
 		for(let args of unlinkArgs) {
-			this.unlink(...args);
+			this.unlisten(...args);
 		}
 
 		return this;
 	}
 
+	emit(data) {
+		for(let listener of this.listeners) {
+			if(listener instanceof Node) {
+				listener.receive(data, this);
+			} else if(typeof listener === "function") {
+				listener(data, this);
+			}
+		}
+
+		return this;
+	}
 	receive(data, sender) {
 		if(this.config.isLocked) {
 			return false;
@@ -66,7 +142,7 @@ export class Node extends AgencyBase {
 		let newState = Object.assign({}, this.state);
 		for(let reducer of this.reducers) {
 			if(typeof reducer === "function") {
-				newState = reducer(data, newState, sender || this);
+				newState = reducer(data, sender, newState);
 			}
 		}
 
@@ -76,13 +152,22 @@ export class Node extends AgencyBase {
 			this.state = newState;
 		}
 
-		for(let listener of this.listeners) {
-			if(listener instanceof Node) {
-				listener.receive(this.state, this);
-			} else if(typeof listener === "function") {
-				listener(this.state, this);
-			}
+		this.emit(this.state);
+
+		return this;
+	}
+
+	selfPropagate() {
+		this.propagate(this.state);
+
+		return this;
+	}
+	propagate(data) {
+		for(let child of this.children) {
+			child.receive(data, this);
 		}
+
+		return this;
 	}
 };
 
